dc89ee21657b9387fac183b0d81d4234a433097f
Antti Keränen <detegr@rbx.email>
2024-06-01 21:17:01 +0300

    OpenXR: Use XR_KHR_vulkan_enable2
    This is the recommended extension for OpenXR initialization. It is also
    mandatory if we're ever going to get for example OpenKneeboard to work
    with openRBRVR in OpenXR mode.

--- src/API.cpp
+++ src/API.cpp
@@ -24,12 +24,89 @@ extern "C" __declspec(dllexport) IPlugin* RBR_CreatePlugin(IRBRGame* game)
     return g::openrbrvr;
 }
 
+extern "C" __declspec(dllexport) VkResult CreateVulkanInstance(VkInstanceCreateInfo* create_info, PFN_vkGetInstanceProcAddr get_instance_proc_addr_fn, VkInstance* vulkan_instance)
+{
+    if (!g::vr || g::vr->get_runtime_type() != OPENXR) {
+        return VK_ERROR_INITIALIZATION_FAILED;
+    }
+
+    OpenXR* vr = reinterpret_cast<OpenXR*>(g::vr);
+    VkResult vulkan_result;
+    XrVulkanInstanceCreateInfoKHR instance_create_info = {
+        .type = XR_TYPE_VULKAN_INSTANCE_CREATE_INFO_KHR,
+        .next = nullptr,
+        .systemId = vr->get_system_id(),
+        .createFlags = 0,
+        .pfnGetInstanceProcAddr = get_instance_proc_addr_fn,
+        .vulkanCreateInfo = create_info,
+        .vulkanAllocator = nullptr,
+    };
+
+    const auto instance = vr->get_instance();
+    auto xrCreateVulkanInstanceKHR = OpenXR::get_extension<PFN_xrCreateVulkanInstanceKHR>(instance, "xrCreateVulkanInstanceKHR");
+    if (auto err = xrCreateVulkanInstanceKHR(instance, &instance_create_info, vulkan_instance, &vulkan_result); err != XR_SUCCESS) {
+        dbg(std::format("Failed to initialize OpenXR: xrCreateVulkanInstanceKHR {}", OpenXR::XrResultToString(instance, err)));
+        return VK_ERROR_INITIALIZATION_FAILED;
+    }
+
+    XrVulkanGraphicsDeviceGetInfoKHR gfx_device_get_info = {
+        .type = XR_TYPE_VULKAN_GRAPHICS_DEVICE_GET_INFO_KHR,
+        .next = nullptr,
+        .systemId = vr->get_system_id(),
+        .vulkanInstance = *vulkan_instance,
+    };
+
+    auto xrGetVulkanGraphicsDevice2KHR = OpenXR::get_extension<PFN_xrGetVulkanGraphicsDevice2KHR>(instance, "xrGetVulkanGraphicsDevice2KHR");
+
+    // This function needs to be called before calling xrCreateVulkanDeviceKHR
+    // The OpenXR runtime might store some internal state (at least PimaxXR does)
+    // even though we're not using the return value for anything here.
+    VkPhysicalDevice vulkan_physical_device;
+    if (auto err = xrGetVulkanGraphicsDevice2KHR(instance, &gfx_device_get_info, &vulkan_physical_device); err != XR_SUCCESS) {
+        dbg(std::format("Failed to initialize OpenXR: xrGetVulkanGraphicsDevice2KHR {}", OpenXR::XrResultToString(instance, err)));
+        return VK_ERROR_INITIALIZATION_FAILED;
+    }
+
+    return vulkan_result;
+}
+
+extern "C" __declspec(dllexport) VkResult CreateVulkanDevice(VkPhysicalDevice vulkan_physical_device, VkDeviceCreateInfo* create_info, PFN_vkGetInstanceProcAddr get_instance_proc_addr_fn, VkDevice* vulkan_device)
+{
+    if (!g::vr || g::vr->get_runtime_type() != OPENXR) {
+        return VK_ERROR_INITIALIZATION_FAILED;
+    }
+
+    OpenXR* vr = reinterpret_cast<OpenXR*>(g::vr);
+
+    XrVulkanDeviceCreateInfoKHR vulkan_device_create_info = {
+        .type = XR_TYPE_VULKAN_DEVICE_CREATE_INFO_KHR,
+        .next = nullptr,
+        .systemId = vr->get_system_id(),
+        .createFlags = 0,
+        .pfnGetInstanceProcAddr = get_instance_proc_addr_fn,
+        .vulkanPhysicalDevice = vulkan_physical_device,
+        .vulkanCreateInfo = create_info,
+        .vulkanAllocator = nullptr,
+    };
+
+    const auto instance = vr->get_instance();
+    auto xrCreateVulkanDeviceKHR = OpenXR::get_extension<PFN_xrCreateVulkanDeviceKHR>(instance, "xrCreateVulkanDeviceKHR");
+
+    VkResult vulkan_result;
+    if (auto err = xrCreateVulkanDeviceKHR(instance, &vulkan_device_create_info, vulkan_device, &vulkan_result); err != XR_SUCCESS) {
+        dbg(std::format("Failed to initialize OpenXR: xrCreateVulkanDeviceKHR {}", OpenXR::XrResultToString(instance, err)));
+        return VK_ERROR_INITIALIZATION_FAILED;
+    }
+
+    return vulkan_result;
+}
+
 extern "C" __declspec(dllexport) int64_t openRBRVR_Exec(ApiOperation ops, uint64_t value)
 {
     dbg(std::format("Exec: {} {}", (uint64_t)ops, (uint64_t)value));
 
     if (ops == API_VERSION) {
-        return 2;
+        return 3;
     }
     if (ops & MOVE_SEAT) {
         if (value > static_cast<uint64_t>(MOVE_SEAT_DOWN)) {
@@ -43,22 +120,6 @@ extern "C" __declspec(dllexport) int64_t openRBRVR_Exec(ApiOperation ops, uint64
     if (ops & TOGGLE_DEBUG_INFO) {
         g::cfg.debug = !g::cfg.debug;
         g::draw_overlay_border = (g::cfg.debug && g::cfg.debug_mode == 0);
-    }
-    if ((ops & OPENXR_REQUEST_INSTANCE_EXTENSIONS) && g::vr && g::vr->get_runtime_type() == OPENXR) {
-        try {
-            OpenXR* vr = reinterpret_cast<OpenXR*>(g::vr);
-            return reinterpret_cast<int64_t>(vr->get_instance_extensions());
-        } catch (const std::runtime_error& e) {
-            MessageBoxA(nullptr, std::format("Could not get OpenXR extensions: {}", e.what()).c_str(), "OpenXR init error", MB_OK);
-        }
-    }
-    if ((ops & OPENXR_REQUEST_DEVICE_EXTENSIONS) && g::vr && g::vr->get_runtime_type() == OPENXR) {
-        try {
-            OpenXR* vr = reinterpret_cast<OpenXR*>(g::vr);
-            return reinterpret_cast<int64_t>(vr->get_device_extensions());
-        } catch (const std::runtime_error& e) {
-            MessageBoxA(nullptr, std::format("Could not get OpenXR extensions: {}", e.what()).c_str(), "OpenXR init error", MB_OK);
-        }
     }
     if (ops & GET_VR_RUNTIME) {
         if (!g::vr) {
